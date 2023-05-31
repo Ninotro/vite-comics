aa69a2afccdedf2182074ebb11a1b7c69df86a40
Ninotro <ninotroia96@gmail.com>
2023-05-31 15:41:49 +0200

    jumbotron

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,86 @@
+<script>
+
+export default {
+  name : "AppHeader",
+
+  data() {
+    return {
+        lists : [
+            "CHARACTERS" ,
+            "COMICS" ,
+            "MOVIES" ,
+            "TV", 
+            "GAMES",
+            "COLLECTIBLES",
+            "VIDEOS",
+            "FANS",
+            "NEWS",
+            "SHOP",
+
+        ]
+    }
+  }
+  
+
+}
+</script>
+
+<template>
+
+    <header>
+    
+        <div class="container-img">
+            <img src="../assets/Immagini/dc-logo.png" alt="logo">
+        </div>
+
+        <div>
+            <ul>
+                <li v-for="list in lists">
+                    <a href="#">{{list}}</a>
+                </li>
+            </ul>
+        </div>
+
+
+    </header>
+
+  
+</template>
+
+<style lang="scss" scoped >
+
+@use "../styles/general.scss" as* ;
+
+header {
+    margin: 30px 10%;
+    
+    height: 200px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    color: black;
+    div {
+
+        ul {
+            display: flex;
+            
+            li{
+                list-style-type: none;
+                padding-right: 10px;
+                font-size: 12px;
+               
+            }
+            
+        }
+        
+    }
+    .container-img {
+        width: 200px;
+        img {
+            object-fit: cover;
+            max-width: 100%;
+        }
+    }
+}
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+  name : "Jumbotron",
+
+  
+  
+
+}
+</script>
+
+<template>
+    <section id="Jumbotron">
+        <div class="container">
+            <p> - - >  Content goes here &lt; - -   </p>
+        </div>
+    </section>
+    
+  
+</template>
+
+<style lang="scss" scoped >
+
+@use "../styles/general.scss" as* ;
+
+#Jumbotron {
+    background-color: black;
+    height: 150px;
+
+    .container {
+        display: flex;
+        align-items: center;
+        
+        p {
+            color: white;
+            font-size: 30px;
+           
+        }
+    }
+}
+
+
+</style>
